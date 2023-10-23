def39f2d42836a90eda8a9fef3102033911338b6
NathanFortyTwo <nathan.feret@gmail.com>
2023-10-23 17:01:24 +0200

    add mpi

--- parallel/mpi_nbody_brute_force.c
+++ parallel/mpi_nbody_brute_force.c
@@ -31,9 +31,37 @@ double sum_speed_sq = 0;
 double max_acc = 0;
 double max_speed = 0;
 
+//MPI
+int *recv_counts;
+int *displacements;
+int rank, size,start,end; 
+
 void init()
 {
-  /* Nothing to do */
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  recv_counts = malloc(sizeof(int) * size);
+  displacements = malloc(sizeof(int) * size);
+  
+  int receive_size = nparticles/size;
+  int remainder = nparticles%size;
+
+  for (int i = 0; i < size; i++) {
+    recv_counts[i] = receive_size;
+    displacements[i] = i*receive_size;
+  }
+  
+  recv_counts[size-1] = receive_size + remainder;
+
+  start = rank * (nparticles / size);
+  end = (rank + 1) * (nparticles / size);
+
+   if (rank == size - 1)
+  {
+     end = nparticles;
+  }
+
 }
 
 #ifdef DISPLAY
@@ -80,6 +108,7 @@ void move_particle(particle_t *p, double step)
   sum_speed_sq += speed_sq;
   max_acc = MAX(max_acc, cur_acc);
   max_speed = MAX(max_speed, cur_speed);
+  
 }
 
 /*
@@ -92,18 +121,7 @@ void all_move_particles(double step, int rank, int size)
 {
   /* First calculate force for particles. */
   int i;
-  // #pragma omp parallel for shared(particles)
 
-  int start = rank * (nparticles / size);
-  int end = (rank + 1) * (nparticles / size);
-
-  // if (rank == size - 1)
-  //{
-  //   end = nparticles;
-  // }
-
-  // on suppose pour l'instant que nparticles est divisible par size
-  assert(end - start == nparticles / size);
 
   for (i = start; i < end; i++)
   {
@@ -117,50 +135,39 @@ void all_move_particles(double step, int rank, int size)
       compute_force(&particles[i], p->x_pos, p->y_pos, p->mass);
     }
   }
+
   double xforces[end - start];
   double yforces[end - start];
+
+  double allxforces[nparticles];
+  double allyforces[nparticles];
+  
   for (i = start; i < end; i++)
   {
     xforces[i - start] = particles[i].x_force;
     yforces[i - start] = particles[i].y_force;
   }
 
-  // gather all forces
   MPI_Barrier(MPI_COMM_WORLD);
+  
+  MPI_Allgatherv(xforces, end - start, MPI_DOUBLE, allxforces, recv_counts, displacements, MPI_DOUBLE, MPI_COMM_WORLD);
+  MPI_Allgatherv(yforces, end - start, MPI_DOUBLE, allyforces, recv_counts, displacements, MPI_DOUBLE, MPI_COMM_WORLD);
+  
 
-  int displs[size];
-  for (i = 0; i < size; i++)
+  for (i = 0; i < nparticles; i++)
   {
-    displs[i] = i * (nparticles / size);
-  }
-
-  double allxforces[nparticles];
-  double allyforces[nparticles];
-
-  // int recvcounts[size];
-  // for (i = 0; i < size; i++)
-  //{
-  //   recvcounts[i] = end - start;
-  // }
-
-  // MPI_Allgatherv(xforces, end - start, MPI_DOUBLE, allxforces, recvcounts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
-  // MPI_Allgatherv(yforces, end - start, MPI_DOUBLE, allyforces, recvcounts, displs, MPI_DOUBLE, MPI_COMM_WORLD);
-
-  MPI_Allgather(xforces, end - start, MPI_DOUBLE, allxforces, end - start, MPI_DOUBLE, MPI_COMM_WORLD);
-  MPI_Allgather(yforces, end - start, MPI_DOUBLE, allyforces, end - start, MPI_DOUBLE, MPI_COMM_WORLD);
-
-  for (i = start; i < end; i++)
-  {
-    particles[i].x_force = allxforces[i - start];
-    particles[i].y_force = allyforces[i - start];
+    particles[i].x_force = allxforces[i];
+    particles[i].y_force = allyforces[i];
   }
 
   /* then move all particles and return statistics */
-  // #pragma omp parallel for shared(particles)
+
   for (i = 0; i < nparticles; i++)
   {
-    move_particle(&particles[i], step);
+    move_particle(&particles[i], step); // fois size mais negligeable
   }
+ 
+  
 }
 
 /* display all the particles */
@@ -187,21 +194,20 @@ void print_all_particles(FILE *f)
 
 void run_simulation(int rank, int size)
 {
-  double t = 0.0, dt = 0.01;
-  while (t < T_FINAL && nparticles > 0)
+  double t = 0.0, dt = 0.01;  
+
+  while (t < T_FINAL && nparticles > 0  )
   {
-    /* Update time. */
     t += dt;
-    /* Move particles with the current and compute rms velocity. */
+    if (!rank){
+    printf("t = %lf\n",t);
+    }
+
     all_move_particles(dt, rank, size);
-
-    /* Adjust dt based on maximum speed and acceleration--this
-       simple rule tries to insure that no velocity will change
-       by more than 10% */
-
     dt = 0.1 * max_speed / max_acc;
 
     /* Plot the movement of the particle */
+
 #if DISPLAY
     clear_display();
     draw_all_particles();
@@ -224,16 +230,9 @@ int main(int argc, char **argv)
     T_FINAL = atof(argv[2]);
   }
 
-  // MPI_Init(&argc, &argv);
-  // int rank;
-  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  init();
-  int rank, size;
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+  init();
+  
   /* Allocate global shared arrays for the particles data set. */
   particles = malloc(sizeof(particle_t) * nparticles);
   all_init_particles(nparticles, particles);
@@ -246,13 +245,13 @@ int main(int argc, char **argv)
 
   struct timeval t1, t2;
   gettimeofday(&t1, NULL);
-
-  /* Main thread starts simulation ... */
   run_simulation(rank, size);
+  MPI_Finalize();
   if (rank)
   {
     return 0;
   }
+
   gettimeofday(&t2, NULL);
 
   double duration = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec) / 1e6);
@@ -281,6 +280,5 @@ int main(int argc, char **argv)
   /* Close the X window used to display the particles */
   XCloseDisplay(theDisplay);
 #endif
-  // MPI_Finalize();
   return 0;
 }
