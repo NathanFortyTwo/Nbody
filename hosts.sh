01ee88f42f9c5e0b23e77733fcc65aa448f7f332
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-23 19:08:31 +0200

    🔨 Add script to generate hosts file for mpi

---
+++ hosts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+reachable=""
+unreachable=""
+
+# Set the SSH timeout in seconds (adjust as needed)
+ssh_timeout=2
+
+for i in {01..13}; do
+    machine="3a401-$i"
+    echo "Testing:" $machine
+    if ssh -q -o BatchMode=yes -o ConnectTimeout=$ssh_timeout $machine exit; then
+        reachable="$reachable $machine"
+    else
+        unreachable="$unreachable $machine"
+    fi
+done
+
+echo "Reachable machines:$reachable"
+echo "Unreachable machines:$unreachable"
+
+# Generate a hosts file with reachable machines
+if [ -n "$reachable" ]; then
+    echo "Generating hosts file with reachable machines..."
+    echo "$reachable" | tr ' ' '\n' >hosts
+    echo "Hosts file 'hosts' created with reachable machines."
+else
+    echo "No reachable machines found."
+fi
