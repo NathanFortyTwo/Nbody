e9e959407393d7ca73af528c77135d644d6ee69e
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-10-23 19:35:43 +0200

    🐛 Fix first blank line script

--- hosts.sh
+++ hosts.sh
@@ -10,11 +10,15 @@ for i in {01..13}; do
     machine="3a401-$i"
     echo "Testing:" $machine
     if ssh -q -o BatchMode=yes -o ConnectTimeout=$ssh_timeout $machine exit; then
-        reachable="$reachable $machine"
+        # Use parameter expansion to remove the leading space
+        reachable="${reachable} $machine"
     else
-        unreachable="$unreachable $machine"
+        unreachable="${unreachable} $machine"
     fi
 done
+
+# Remove the leading space from the reachable machines list
+reachable="${reachable# }"
 
 echo "Reachable machines:$reachable"
 echo "Unreachable machines:$unreachable"
