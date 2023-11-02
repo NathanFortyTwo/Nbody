fbdd0f670d761e3373bd462f1af73fa3d2b13333
NathanFortyTwo <nathan.feret@gmail.com>
2023-11-02 23:02:10 +0100

    rename parallel to src

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 ## Télécom SudParis CSC-5001 : High Performance Systems
 
 Parallelisation de différents algorithmes
-(brute force et Barnes-Hut) pour le calcul de la force gravitationnelle entre N corps.
+(Brute force et Barnes-Hut) pour le calcul de la force gravitationnelle entre N corps.
 
 [Le sujet du projet](https://www-inf.telecom-sudparis.eu/COURS/CSC5001/Supports/Projet/Projet2022/NBody/sujet.php)
 
+Le code source est dans le dossier `src/`
+Les fichiers contenant les différentes solutions sont :
 
+- omp_nbody_brute_force.c
+- mpi_nbody_brute_force.c
+- cuda_nbody_brute_force.cu
 
-## Avant de commit:
-```bash
-make clean
-```
- 
+Les résultats des temps d'exécution sont dans le dossier `runtimes/`.
+Les temps sont calculés pour t=2.
--- runtimes/run.sh
+++ runtimes/run.sh
@@ -4,13 +4,13 @@ rm particles.log
 
 for nbparticule in 500 1000 1500 2000 2500;  do
 
-    ../parallel/sequential_nbody_brute_force $nbparticule $total_time | grep "Simulation took" | awk '{print $3}' >> "sequential_nbody_brute_force.txt"
-    ../parallel/sequential_nbody_barnes_hut $nbparticule $total_time | grep "Simulation took" | awk '{print $3}' >> "sequential_nbody_barnes_hut.txt"
+    ../src/sequential_nbody_brute_force $nbparticule $total_time | grep "Simulation took" | awk '{print $3}' >> "sequential_nbody_brute_force.txt"
+    ../src/sequential_nbody_barnes_hut $nbparticule $total_time | grep "Simulation took" | awk '{print $3}' >> "sequential_nbody_barnes_hut.txt"
 
     for nb_thread in 2 4 6; do
-        mpirun -np $nb_thread --hostfile hosts ../parallel/mpi_nbody_brute_force $nbparticule $total_time | grep "Simulation took" | awk '{print $3}' >> "mpi_nbody_brute_force_$nb_thread.txt"
+        mpirun -np $nb_thread --hostfile hosts ../src/mpi_nbody_brute_force $nbparticule $total_time | grep "Simulation took" | awk '{print $3}' >> "mpi_nbody_brute_force_$nb_thread.txt"
         export OMP_NUM_THREADS=$nb_thread
-        ../parallel/omp_nbody_brute_force $nbparticule $total_time| grep "Simulation took" | awk '{print $3}' >> "omp_nbody_brute_force_$nb_thread.txt"
+        ../src/omp_nbody_brute_force $nbparticule $total_time| grep "Simulation took" | awk '{print $3}' >> "omp_nbody_brute_force_$nb_thread.txt"
     done
 
 done
